f3bb0626f0715d59dfdbd1458e7c08b99a39126a
Sajidhgn <sajidhgn@gmail.com>
2023-09-25 15:31:19 +0500

    Assignment psd generator

--- lib/pasgen_pro_web/controllers/page_controller.ex
+++ lib/pasgen_pro_web/controllers/page_controller.ex
@@ -3,26 +3,56 @@ defmodule PasgenProWeb.PageController do
 
   alias PasgenPro.Pasgen.PasGen
 
+  @number_chars "0123456789"
+  @symbol_chars "!@#$%^&*()_+-=[]{}|;:'\",.<>/?"
+  @uppercase_chars "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
   def home(conn, _params) do
     changeset = PasGen.changeset(%PasGen{}, %{})
-    list = %{
-      "length_field" => "1",
-      "number_field" => "true",
-      "symbol_field" => "true",
-      "uppercase_field" => "true"
-    }
+    list = %{"password_field" => "120358yi", "length_field" => 1}
     render(conn, :home, list: list, changeset: changeset)
   end
 
-  
+
   def create(conn, %{"pas_gen" => list}) do
     changeset =  PasGen.changeset( %PasGen{}, list)
+
     if changeset.valid? do
-      conn
-      |> render(:home, list: list, changeset: changeset)
+    length = Map.get(list, "length_field") |> String.to_integer()
+    number = if list["number_field"]=="true", do: "true", else: ""
+    uppercase = if list["uppercase_field"]=="true", do: "true", else: ""
+    symbol = if list["symbol_field"]=="true", do: "true", else: ""
+  {:ok, password} = generate_password(length, symbol, uppercase, number)
+      conn |> render(:home, list: %{"password_field" => password, "symbol_field" => symbol, "length_field" => length,"number_field" => number, "uppercase_field" => uppercase}, changeset: changeset)
     else
       conn
-      |> render(:home, list: list, changeset: changeset)
+      |> render(:home, changeset: changeset)
     end
+  end
+
+  def generate_password(length, use_symbols, use_uppercase, use_numbers) do
+    charset =
+      String.trim_trailing(
+        (if use_symbols, do: @symbol_chars, else: "") <>
+        (if use_uppercase, do: @uppercase_chars, else: "") <>
+        (if use_numbers, do: @number_chars, else: "")
+      )
+
+      if length <= String.length(charset) do
+        password = generate_password_recursive(charset, length, "")
+        {:ok, password}
+      else
+        {:error, "Invalid parameter types. Please ensure that number, uppercase, and symbol are booleans, and length is an integer."}
+      end
+
+  end
+
+  defp generate_password_recursive(_, 0, password) do
+    password
+  end
+  defp generate_password_recursive(alphabet, length, password) do
+    random_index = :rand.uniform(String.length(alphabet))
+    random_char = String.at(alphabet, random_index)
+    generate_password_recursive(alphabet, length - 1, password <> random_char)
   end
 end
